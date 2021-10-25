839381dbd046f5c39f5e13dfa93ca0753d26d2c4
Shubham Dutt <38075319+ShubhamDutt@users.noreply.github.com>
2021-10-25 12:40:45 +0530

    Add files via upload

--- model/Train.py
+++ model/Train.py
@@ -4,6 +4,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+import requests
+import json
+
 
 def train_model():
     iris_df = datasets.load_iris()
@@ -18,3 +21,24 @@ def train_model():
     accuracy = accuracy_score(y_test, preds)
     joblib.dump(dt, 'iris-model.model')
     print(f'Model Training Finished.\n\tAccuracy obtained: {accuracy}')
+
+
+# heroku url
+heroku_url = 'https://iris-prediction-flask.herokuapp.com/'  # change to your app name
+
+# test data
+data = {'sepal_length': 7.2
+    , 'sepal_width': 5.3
+    , 'petal_length': 2.5
+    , 'petal_width': 1.1}
+
+data = json.dumps(data)
+print(data)
+
+# check response code
+r_survey = requests.post(heroku_url, data)
+print(r_survey)
+
+send_request = requests.post(heroku_url, data)
+print(send_request)
+print(send_request.json())
